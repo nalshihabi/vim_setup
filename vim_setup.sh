4412c197053c85b37b72589e9bac2d29957a32fa
Noah AL-Shihabi <noahshihabi@gmail.com>
2019-12-25 17:12:06 -0500

    Setup

---
+++ vim_setup.sh
@@ -0,0 +1,19 @@
+# Update and upgrade
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Copy over vimfiles
+mkdir ~/.vim
+cp colors.vim ~/.vim
+cp filetypes.vim ~/.vim
+cp extended.vim ~/.vim
+cp .vim
+
+# Get Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Setup YouCompleteMe
+sudo apt install build-essential cmake3 python3-dev
+cd ~/.vim/bundle/YouCompleteMe
+python3 install.py --clang-completer
+python3 install.py --all
